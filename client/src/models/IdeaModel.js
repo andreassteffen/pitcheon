ec76426f3af7d619f0fa1c4e91261df9c7f4dd0e
Andreas Steffen <Andreas.Steffen@gmail.com>
2016-10-09 22:54:32 +0200

    added mobx

---
+++ client/src/models/IdeaModel.js
@@ -0,0 +1,40 @@
+import {observable} from 'mobx';
+
+export default class IdeaModel {
+	constructor(store, id, title, description, customer) {
+		this.store = store;
+		this.id = id;
+		this.title = observable(title);
+		this.description = observable(description);
+		this.customer = observable(customer);
+	}
+
+	destroy() {
+		this.store.ideas.remove(this);
+	}
+
+	setTitle(title) {
+		this.title = title;
+	}
+
+	setDescription(description) {
+		this.description = description;
+	}
+
+	setCustomer(customer) {
+		this.customer = customer;
+	}
+	
+	toJS() {
+		return {
+			id: this.id,
+			title: this.title,
+			description: this.description,
+			customer: this.customer
+		};
+	}
+
+	static fromJS(store, object) {
+		return new IdeaModel(store, object.id, object.title, object.description, object.customer);
+	}
+}
